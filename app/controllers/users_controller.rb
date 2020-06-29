18899868d07c2d475f725920d6326e53b8987f85
cantstopcoding <jonathan_koss@yahoo.com>
2020-06-28 21:23:39 -0400

    find user in post /login by email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    post '/login' do 
+        # find user
+        user = User.find_by(email: params[:email])
+        # authenticate the user
+        # log them in
+        # redirect user's profile (user's show)
+    end
+
 end
