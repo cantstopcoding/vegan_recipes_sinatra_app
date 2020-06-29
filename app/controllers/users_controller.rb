1b8aacac9690884933c1fbd5e319def8ffe60b62
cantstopcoding <jonathan_koss@yahoo.com>
2020-06-28 23:40:44 -0400

    create a kay/value pair for session hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,16 @@ class UsersController < ApplicationController
         if !!user && user.authenticate(params[:password])
             # log them in
             # create a user key/value pair in the session hash for the user if they actually get logged in
+            session[:user_id] = user.id
             binding.pry
             # redirect user's profile (user's show)
+
         else  
             # show error message
             redirect '/login'
         end 
     end
+
+    
 
 end
