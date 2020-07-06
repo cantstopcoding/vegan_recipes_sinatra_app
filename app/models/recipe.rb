1848501d339274fa4e4388bfafdcc45355fc5bb3
cantstopcoding <jonathan_koss@yahoo.com>
2020-07-05 21:19:38 -0400

    add slug methods to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ActiveRecord::Base
     belongs_to :user
 
     validates :title, :image_url, :ingredients, :instructions, presence: true
+
+    def slug
+        name.downcase.gsub(" ","-")
+    end
+    
+    def self.find_by_slug(slug)
+        Recipe.all.find{|artist| artist.slug == slug}
+    end
 end
