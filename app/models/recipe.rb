9373a8e6af3bbb989a6484cc8b600823f50bfd36
cantstopcoding <jonathan_koss@yahoo.com>
2020-07-07 12:13:04 -0400

    delete all slugify methodology

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,4 @@ class Recipe < ActiveRecord::Base
     belongs_to :user
 
     validates :title, :image_url, :ingredients, :instructions, presence: true
-
-    def slug
-        name.downcase.gsub(" ","-")
-    end
-    
-    def self.find_by_slug(slug)
-        Recipe.all.find{|artist| artist.slug == slug}
-    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,4 @@ class User < ActiveRecord::Base
     has_secure_password
 
     has_many :recipes
-
-    def slug
-        name.downcase.gsub(" ","-")
-    end
-    
-    def self.find_by_slug(slug)
-        User.all.find{|artist| artist.slug == slug}
-    end
 end
